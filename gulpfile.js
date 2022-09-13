3e675b406b0508b31cf6fab45486f59d2c789481
qualley <matt.qualley93@gmail.com>
2022-09-13 16:14:04 -0400

    updating repo

--- gulpfile.js
+++ gulpfile.js
@@ -24,5 +24,6 @@ function watch() {
     gulp.watch('./scss/**/*.scss', compilescss);
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
+
 exports.compilescss = compilescss;
 exports.watch = watch;
